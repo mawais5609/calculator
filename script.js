460590f697a7abcfa74d6ce9b9cfa6a052a86bff
mawais5609 <mawais5609@gmail.com>
2026-02-16 23:24:28 +0500

    Implemented operate function and tested with all calculator functions.

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+function operate(num1, operator, num2) {
+    switch (operator){
+        case "+":
+            return add(num1, num2)
+
+        case "-":
+            return subtract(num1, num2)
+
+        case "x":
+            return multiply(num1, num2)
+
+        case "/":
+            return divide(num1, num2)
+        
+        default:
+            return "Invalid Operator."
+    }
+}
+
+console.log(operate(20,"x",10))
