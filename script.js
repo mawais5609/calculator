e21ff8fbbe0600d1eb69dbfed56fa1b63c605da7
mawais5609 <mawais5609@gmail.com>
2026-02-17 19:11:38 +0500

    Created basic html content and styles now implementing javascript to make everything interactive.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const numbersContainer = document.querySelector(".numbers")
+const operatorsContainer = document.querySelector(".operators")
+const customButtonsContainer = document.querySelector(".custom-buttons")
+const display = document.querySelector('.display')
+
+let firstNumber = ""
+let secondNumber = ""
+let operator = null
+
+numbersContainer.addEventListener("click", (event) => {
+})
+
+operatorsContainer.addEventListener("click", (event) => {
+})
+
+customButtonsContainer.addEventListener("click", (event) => {
+})
+
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -15,7 +34,7 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operator, num2) {
-    switch (operator){
+    switch (operator) {
         case "+":
             return add(num1, num2)
 
@@ -27,10 +46,8 @@ function operate(num1, operator, num2) {
 
         case "/":
             return divide(num1, num2)
-        
+
         default:
             return "Invalid Operator."
     }
 }
-
-console.log(operate(20,"x",10))
