eefb68c69e9d39a49d6e712e0c7541033a4948a5
mawais5609 <mawais5609@gmail.com>
2026-02-18 02:12:41 +0500

    Calculator project completed implemented every step for the buttons to work properly.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # calculator
+
+This is my 3rd project from the odin project.
+Its a basic calculator with basic calculator methods.
+Every Part of the calculator is workable.
+If there's any issue with any part of my calculator please inform me.
--- script.js
+++ script.js
@@ -5,17 +5,116 @@ const display = document.querySelector('.display')
 
 let firstNumber = ""
 let secondNumber = ""
-let operator = null
+let currentOperator = null
+let justCalculated = false
 
 numbersContainer.addEventListener("click", (event) => {
+    const targetNumber = event.target.getAttribute("data-value")
+
+    if (targetNumber === null) {
+        return
+    }
+
+    if (justCalculated && currentOperator === null) {
+        firstNumber = targetNumber
+        display.textContent = firstNumber
+        
+        justCalculated = false
+        secondNumber = ""
+        return
+    }
+
+    if (currentOperator === null) {
+        firstNumber += targetNumber
+        display.textContent = firstNumber
+    } else {
+        secondNumber += targetNumber
+        display.textContent = secondNumber
+    }
+
 })
 
 operatorsContainer.addEventListener("click", (event) => {
+    const targetOperator = event.target.getAttribute("data-value")
+
+    if (targetOperator === null) {
+        return
+    }
+
+    else if (currentOperator !== null) {
+        let result = operate(Number(firstNumber), currentOperator, Number(secondNumber))
+
+        if (!Number.isInteger(result)) result = Number(result.toFixed(2))
+
+        display.textContent = result
+
+        firstNumber = result
+        secondNumber = ""
+        currentOperator = null
+    }
+
+    currentOperator = targetOperator
 })
 
 customButtonsContainer.addEventListener("click", (event) => {
-})
+    const targetCustomButton = event.target.getAttribute("id")
 
+    if (targetCustomButton === null) {
+        return
+    }
+
+    if (targetCustomButton === "equals") {
+
+        if (firstNumber === "" || secondNumber === "" || currentOperator === null) {
+            return
+        }
+
+        let result = operate(Number(firstNumber), currentOperator, Number(secondNumber))
+
+        if (!Number.isInteger(result)) result = Number(result.toFixed(2))
+
+        display.textContent = result
+
+        firstNumber = result
+        secondNumber = ""
+        currentOperator = null
+        justCalculated = true
+
+    }
+
+    if (targetCustomButton === "clear") {
+        display.textContent = "0"
+        firstNumber = ""
+        secondNumber = ""
+        currentOperator = null
+        justCalculated = false
+    }
+
+    if (targetCustomButton === "del") {
+        if (currentOperator === null) {
+            firstNumber = firstNumber.slice(0, -1)
+            display.textContent = firstNumber
+        } else {
+            secondNumber = secondNumber.slice(0, -1)
+            display.textContent = secondNumber
+        }
+    }
+
+    if (targetCustomButton === "change-sign") {
+
+        if (currentOperator === null && firstNumber !== "") {
+            firstNumber = firstNumber === -firstNumber ? firstNumber : -firstNumber
+            display.textContent = firstNumber
+        } 
+        else if (currentOperator !== null && secondNumber !== "") {
+            secondNumber = secondNumber === -secondNumber ? secondNumber : -secondNumber
+            display.textContent = secondNumber
+        }
+
+    }
+
+
+})
 
 function add(num1, num2) {
     return num1 + num2
@@ -35,19 +134,14 @@ function divide(num1, num2) {
 
 function operate(num1, operator, num2) {
     switch (operator) {
-        case "+":
-            return add(num1, num2)
+        case "+": return add(num1, num2)
 
-        case "-":
-            return subtract(num1, num2)
+        case "-": return subtract(num1, num2)
 
-        case "x":
-            return multiply(num1, num2)
+        case "x": return multiply(num1, num2)
 
-        case "/":
-            return divide(num1, num2)
+        case "/": return divide(num1, num2)
 
-        default:
-            return "Invalid Operator."
+        default: return "Invalid Operator."
     }
 }
